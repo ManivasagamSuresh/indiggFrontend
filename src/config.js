648c911d1c9bbfff5df178d9b4a86839dc325425
Manivasagam Suresh <manivasagam.suresh12@gmail.com>
2023-08-13 23:21:00 +0530

    url changed

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+// export const Config = {
+//     "api": "http://localhost:5000/api",
+// }
 export const Config = {
-    "api": "http://localhost:5000/api",
+    "api": "https://indigg-1i0o.onrender.com",
 }
